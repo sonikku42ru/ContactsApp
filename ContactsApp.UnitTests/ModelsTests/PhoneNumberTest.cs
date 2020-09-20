c11b85bebe912a95b324a1001ddca971d8b3a2a9
Sonikku Za Hejihoggu <sonic42ru@gmail.com>
2020-09-21 03:18:35 +0700

    <PhoneNumberTest.cs>:Добавлен юнит-тест для номера телефона.

---
+++ ContactsApp.UnitTests/ModelsTests/PhoneNumberTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ContactsApp.Models;
+using NUnit.Framework;
+
+namespace ContactsApp.UnitTests.ModelsTests
+{
+    [TestFixture]
+    public class PhoneNumberTest
+    {
+        private PhoneNumber _phoneNumber;
+
+        [SetUp]
+        public void Init()
+        {
+            _phoneNumber = new PhoneNumber();
+        }
+
+        [Test(Description = "Positive Number getter test")]
+        public void TestNumberGet_CorrectValue()
+        {
+            long expected = 79998881234;
+            _phoneNumber.Number = expected;
+            Assert.AreEqual(expected,_phoneNumber.Number, "Number getter returned unexpected value");
+        }
+
+        [TestCase("9091240912049240",
+            "An attempt to assign too long value should result in an exception.",
+            TestName = "Negative Number setter test: trying to set too long number")]
+        [TestCase("12345",
+            "An attempt to assign too short value should result in an exception.",
+            TestName = "Negative Number setter test: trying to set too short number")]
+        [TestCase("-9000",
+            "An attempt to assign negative value should result in an exception.",
+            TestName = "Negative Number setter test: trying to set negative number")]
+        public void TestNumberSet_IncorrectNumber(string incorrect, string message)
+        {
+            long wrong = long.Parse(incorrect);
+            Assert.Throws<ArgumentException>(() => { _phoneNumber.Number = wrong; });
+        }
+    }
+}
--- ContactsApp/Models/PhoneNumber.cs
+++ ContactsApp/Models/PhoneNumber.cs
@@ -7,16 +7,16 @@ namespace ContactsApp.Models
     /// </summary>
     public class PhoneNumber
     {
-        private long _phoneNumber = 0;
-        private const int MaxDigits = 11;
+        private long _phoneNumber = 70000000000;
+        private const int Length = 11;
 
         public long Number
         {
             get => _phoneNumber;
             set
             {
-                if (Digits(value) < MaxDigits)
-                    throw new ArgumentException("Insufficient digits in phone number");
+                if (Digits(value) != Length)
+                    throw new ArgumentException("Number must be 11 digits.");
                 _phoneNumber = value;
             }
         }
@@ -25,7 +25,7 @@ namespace ContactsApp.Models
         {
             if (phone == 0)
                 return 1;
-            return (int) Math.Ceiling(Math.Log10(Math.Abs(phone) + 0.5));
+            return (int)Math.Ceiling(Math.Log10(Math.Abs(phone) + 0.5));
         }
     }
 }
