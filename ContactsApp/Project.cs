6ee6a609ef870c37eaa0cce11a47a51c39131205
Sonikku Za Hejihoggu <sonic42ru@gmail.com>
2020-10-05 15:39:49 +0700

    <Project.cs>:Добавлена возможность сортировки и поиска контактов.

--- ContactsApp/Project.cs
+++ ContactsApp/Project.cs
@@ -6,7 +6,14 @@ namespace ContactsApp
 {
     public class Project
     {
-        public List<Contact> Contacts = new List<Contact>();
+        public List<Contact> Contacts { get; set; } = new List<Contact>();
+
+        public List<Contact> GetSorted() => Contacts.OrderBy(i => i.LastName).ToList();
+
+        public List<Contact> FindSorted(string query) => Contacts
+            .Where(i => i.LastName.Contains(query))
+            .OrderBy(j => j.LastName)
+            .ToList();
 
         public bool Equals(Project other)
         {
