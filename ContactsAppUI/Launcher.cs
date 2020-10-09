a5eda2e425e7146a4a36f70690e98b0a7ec2e9d8
Sonikku Za Hejihoggu <sonic42ru@gmail.com>
2020-10-09 18:48:50 +0700

    <Launcher.cs>:Обработка ошибок при запуске приложения.

--- ContactsAppUI/Launcher.cs
+++ ContactsAppUI/Launcher.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ContactsApp.Exceptions;
 using ContactsAppUI.Forms;
 using ContactsAppUI.ViewModels;
 
@@ -8,12 +9,24 @@ namespace ContactsAppUI
 {
     public class Launcher
     {
+        /// <summary>
+        /// Загрузка контактов и основной формы
+        /// </summary>
+        /// <returns></returns>
         public async Task StartApplication()
         {
-            var contactsViewModel = new ContactsViewModel();
-            await contactsViewModel.LoadProject();
-            var contactsForm = new ContactsForm(contactsViewModel);
-            Application.Run(contactsForm);
+            try
+            {
+                var contactsViewModel = new ContactsViewModel();
+                await contactsViewModel.LoadProject();
+                var contactsForm = new ContactsForm(contactsViewModel);
+                Application.Run(contactsForm);
+            }
+            catch (ProjectReadingException ex)
+            {
+                var messageBox = MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK);
+                Application.Exit();
+            }
         }
 
         #region Signleton
